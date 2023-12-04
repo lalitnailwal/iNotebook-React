57d263e0c6d3da0fd9db03451203c541321fdbd7
lalitnailwal <36082917+lalitnailwal@users.noreply.github.com>
2023-12-04 13:15:38 +0530

    Creating Routes and Schema for Storing Notes V-44 (part 2)

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,11 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    obj = {
-        a: 'thios',
-        number: 34
-    }
-    res.json(obj);
+    console.log(req.body);
+    res.send("ok")
 })
 
 module.exports = router;
